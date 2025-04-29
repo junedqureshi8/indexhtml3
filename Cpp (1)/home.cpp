913e8b1dc77e6c4d7736a065cd2b4f722389f7b8
junedqureshi8 <mohammadjuned765u@gmail.com>
2025-04-29 13:01:58 +0530

    first commit

---
+++ Cpp (1)/home.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+   // int a=20,b=50;
+     // a=a+b;
+    //cout<<a<<endl;
+//    a+=b;
+    //cout<<a;
+// a*=b;
+  // cout<<a;
+// b/=a;
+   //cout<<b;
+}
+
+
+
+
---
+++ Cpp (1)/pattern.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std ;
+ 
+
+// // //  int main()
+// // // {
+
+// // // int i=1;
+
+// // // while(i<=10){
+
+
+// // //      cout<<i<<endl;
+// // //      i++;
+// // // }
+// // // }
+
+// // int main()
+// // {
+// //     for(int i=1;i<=10;i++){
+// //         cout<<i<<endl;
+// //     }
+
+// // }
+// int main()
+// {
+
+//     int num,ans=0;
+//     cout<<"enter a number:";
+//     cin>>num;
+
+//     for(int i=1;i<=num;i++){
+
+//         ans=ans+i;
+
+//     }
+//     cout<<ans;
+// }
+
+int main(){        
+
+int num,ans=1;
+cout<<"enter a number";
+cin>>num;
+
+for(int i=1;i<=num;i++){
+
+    ans=ans*i;
+
+}
+cout<<ans;
+
+}
